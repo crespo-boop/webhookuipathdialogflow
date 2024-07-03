cecc1abe46d5ddaed7a0dfb3bd6571eb7e232306
crespo-boop <crespojean200@gmail.com>
2024-07-03 15:53:04 -0500

    render 4

--- server.js
+++ server.js
@@ -15,20 +15,22 @@ const BOT_JSON = {
     overrideDefaultDevice: false,
     botInput: {}
 };
-const CR_USERNAME = 'e1315299923@live.uleam.edu.ec'
-const CR_PASSWORD = 'Jerick2001.'
+const CR_USERNAME = 'e1315299923@live.uleam.edu.ec';
+const CR_PASSWORD = 'Jerick2001.';
 
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-    const { cedula } = req.body.queryResult.parameters;  // Ajusta según la estructura de tu payload JSON
+    // Obtener los parámetros necesarios del cuerpo de la solicitud JSON
+    const { cedula } = req.body.queryResult.parameters;
 
     // Añadir la cedula al JSON del bot
     BOT_JSON.botInput.cedula = { type: "STRING", string: cedula };
 
-    // Obtener token para acceder al Control Room
+    // Obtener token para acceder al Control Room de Automation Anywhere
     const authPayload = { username: CR_USERNAME, password: CR_PASSWORD };
     const authUrl = `${CONTROL_ROOM}/v1/authentication`;
+    
     request.post(authUrl, { json: authPayload }, (error, response, body) => {
         if (error) {
             return res.json({ fulfillmentText: "Error al obtener el token." });
@@ -42,15 +44,13 @@ app.post('/webhook', (req, res) => {
                 return res.json({ fulfillmentText: "Error al desplegar el bot." });
             }
             return res.json({
-                fulfillmentText: "Gracias por la información. Tu referencia de solicitud es MORT001034. Uno de nuestros consultores te contactará pronto."
+                fulfillmentText: "Gracias por la información. Tu solicitud está siendo procesada."
             });
         });
     });
-});
-app.get('/webhook', (req, res) => {
-    res.send('Webhook está funcionando correctamente.');
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor web escuchando en http://localhost:${PORT}`);
 });
+
