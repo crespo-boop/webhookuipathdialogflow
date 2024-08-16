59f280704fe33edb6a3ff00c9c0f80b6d8423b67
crespo-boop <crespojean200@gmail.com>
2024-08-15 21:31:18 -0500

    3377466000477793355599983

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post('/webhook', async (req, res) => {
 
         try {
             // Configuración de UiPath Orchestrator
-            const authToken = 'tu_token_de_autenticacion';
-            const processUrl = 'https://tu_url_de_uipath';
+            const authToken = 'rt_34C199550857FECB0FC5E0390130D76F724E921330095B52621DD4028AC9760A-1';
+            const processUrl = 'https://cloud.uipath.com/uleam_proyecto/DefaultTenant/orchestrator_/odata/Queues/UiPathODataSvc.AddQueueItem';
 
             const response = await axios.post(processUrl, jobData, {
                 headers: {
